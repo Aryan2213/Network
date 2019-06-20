f57891dc792306566f74d44e543cfdb82b7f4941
Deathy13 <36681172+Deathy13@users.noreply.github.com>
2019-06-20 14:21:09 +1000

    add the Network

---
+++ ComplexGameSystems-master/ComplexGameSystems-master/Assets/UNET/Scripts/Controlers/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 75f;
+    public Rigidbody rigid;
+    public GameObject cam;
+
+
+    // Update is called once per frame
+    public void Move(float inputH, float inputV)
+    {
+
+
+        Vector3 force = new Vector3(inputH, 0, inputV);
+        force = transform.TransformDirection(force);
+        rigid.AddForce(force * speed);
+        Vector3 euler = cam.transform.eulerAngles;
+        transform.rotation = Quaternion.AngleAxis(euler.y, Vector3.up);
+
+    }
+}
+
---
+++ ComplexGameSystems-master/ComplexGameSystems-master/Assets/UNET/Scripts/Inputs/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerController controller;
+    public Orbit cam;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        controller.Move(inputH, inputV);
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        cam.Look(mouseX, mouseY);
+    }
+}
